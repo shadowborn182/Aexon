cab6b241f63a32b23586f7ad696e4892309c4520
shadowborn182 <shadowborn18200p@gmail.com>
2026-05-03 17:24:22 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,10 +24,10 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        // Text users will see (example: "Playing /help | Aexon Bot").
+        name: "/LastNight",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
